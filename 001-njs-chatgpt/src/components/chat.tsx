cdce954bde1827963687e94e14d44fcea343539d
VLAD EMINOVICI <veminovici@hotmail.com>
2023-11-02 11:02:42 +0200

    Basic chat UI

--- 001-njs-chatgpt/src/components/chat.tsx
+++ 001-njs-chatgpt/src/components/chat.tsx
@@ -1,9 +1,27 @@
+"use client"
+
 import React from 'react'
 
 export default function Chat() {
   return (
     <div>
-      <h1>Hello</h1>
+      {/* Text messages */}
+      <div>
+        <h3 className='text-lg font-semibold mt-2'>GPT-4</h3>
+        <p>I am a robot with GPT-4</p>
+      </div>
+      <div>
+        <h3 className='text-lg font-semibold mt-2'>User</h3>
+        <p>I am an user</p>
+      </div>
+
+      <form className='mt-12'>
+        <p>User Message</p>
+        <textarea className='mt-2 w-full bg-slate-800 p-2' placeholder={"What are data structures and algorithms?"}></textarea>
+        <button className='rounded-md bg-blue-600 p-2 mt-2'>
+          Send Message
+        </button>
+      </form>
     </div>
   )
 }
