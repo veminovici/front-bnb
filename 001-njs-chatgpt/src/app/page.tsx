63bd7cc2f2c8b55d00647426a99e3d1b1d89a6b4
VLAD EMINOVICI <veminovici@hotmail.com>
2023-11-02 10:53:30 +0200

    Init the main page and chat component

--- 001-njs-chatgpt/src/app/page.tsx
+++ 001-njs-chatgpt/src/app/page.tsx
@@ -1,8 +1,13 @@
+import Chat from '@/components/chat'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className='bg-slate-800 p-3 w-[800px] rounded-md text-white'>
+        <h2 className='text-2xl'>GPT4 Streaming Chat Application</h2>
+        <Chat />
+      </div>
     </main>
   )
 }
