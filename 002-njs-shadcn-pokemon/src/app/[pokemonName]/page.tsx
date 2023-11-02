a9b680f1f69f3b62987ce323a4078108bec921ce
VLAD EMINOVICI <veminovici@hotmail.com>
2023-11-02 14:41:33 +0200

    Add imagine of the pokemon

--- 002-njs-shadcn-pokemon/src/app/[pokemonName]/page.tsx
+++ 002-njs-shadcn-pokemon/src/app/[pokemonName]/page.tsx
@@ -1,5 +1,6 @@
 import { Progress } from '@/components/ui/progress';
 import { getPokemon } from '@/lib/pokemon-api';
+import Image from 'next/image';
 import React from 'react'
 
 export default async function PokemonPage({ params } : { params: { pokemonName: string } }) {
@@ -12,6 +13,7 @@ export default async function PokemonPage({ params } : { params: { pokemonName: 
   return (
     <>
       <h1 className="text-4xl text-bold pt-4">{pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h1>
+      <Image src={pokemonObject.sprites.front_default} width="200" height="200" alt={pokemonName}/>
       <h3>Weight: {pokemonObject.weight}</h3>
       <div className="flex-col">
                 {pokemonObject.stats.map( (statObject : any) => {
