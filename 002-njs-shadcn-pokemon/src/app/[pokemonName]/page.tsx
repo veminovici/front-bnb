dad9d5f59022438f15a442a2bcd27b79081de2a0
VLAD EMINOVICI <veminovici@hotmail.com>
2023-11-02 14:37:09 +0200

    Get the pokemon details

--- 002-njs-shadcn-pokemon/src/app/[pokemonName]/page.tsx
+++ 002-njs-shadcn-pokemon/src/app/[pokemonName]/page.tsx
@@ -1,11 +1,31 @@
+import { Progress } from '@/components/ui/progress';
+import { getPokemon } from '@/lib/pokemon-api';
 import React from 'react'
 
 export default async function PokemonPage({ params } : { params: { pokemonName: string } }) {
   const { pokemonName } = params;
 
+  const pokemonObject = await getPokemon(pokemonName);
+
+  console.log(pokemonObject);
+  
   return (
     <>
       <h1 className="text-4xl text-bold pt-4">{pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h1>
+      <h3>Weight: {pokemonObject.weight}</h3>
+      <div className="flex-col">
+                {pokemonObject.stats.map( (statObject : any) => {
+                    const statName = statObject.stat.name;
+                    const statValue = statObject.base_stat;
+
+                    return (
+                        <div className="flex items-stretch" style={{width: "500px"}} key={statName}>
+                            <h3 className="p-3 w-2/4">{statName}: {statValue}</h3>
+                            <Progress className="w-2/4 m-auto" value={statValue} />
+                        </div>
+                    )
+                })}
+            </div>
     </>
   )
 }
