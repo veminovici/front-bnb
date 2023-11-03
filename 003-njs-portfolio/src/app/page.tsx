0d9d60cd7c85fbe376c60ad23c35cc05a4e1ee4c
VLAD EMINOVICI <veminovici@hotmail.com>
2023-11-03 15:21:35 +0200

    Add section devider

--- 003-njs-portfolio/src/app/page.tsx
+++ 003-njs-portfolio/src/app/page.tsx
@@ -1,9 +1,11 @@
+import SectionDivider from '@/components/section-divider';
 import Intro from '@/components/section-intro';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center px-4'>
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
