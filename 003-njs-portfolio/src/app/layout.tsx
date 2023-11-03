a005c4955be66bb8abfc503795c6f8d6e4aa094f
VLAD EMINOVICI <veminovici@hotmail.com>
2023-11-03 14:55:04 +0200

    Add header

--- 003-njs-portfolio/src/app/layout.tsx
+++ 003-njs-portfolio/src/app/layout.tsx
@@ -1,29 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import BackgroundRed from '@/components/background-red'
-import BackgroundBlue from '@/components/background-blue'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import BackgroundRed from '@/components/background-red';
+import BackgroundBlue from '@/components/background-blue';
+import Header from '@/components/header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Ricardo | Personal Portfolio',
   description: 'Ricardo is a full-stack developer',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-      className={`${inter.className} relative bg-gray-50 text-gray-950`}>
+    <html lang='en'>
+      <body className={`${inter.className} relative bg-gray-50 text-gray-950`}>
         <BackgroundRed />
         <BackgroundBlue />
+        <Header />
         {children}
-        </body>
+      </body>
     </html>
-  )
+  );
 }
